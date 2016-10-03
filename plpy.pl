2de4c55395060a6bf7a4e07277a050105e25289d
Paul Walters <z5077446@student.unsw.edu.au>
2016-10-04 02:03:39 +1100

    04/09/2016 01:50-02:03 Made an amendment to plpy.pl tp remove a redundant file dependency that blocked submission

--- plpy.pl
+++ plpy.pl
@@ -1,81 +1,29 @@
 #!/usr/bin/perl -w
 
 # plpy.pm
-# Created by Paul Walters z5077446
+# Created by Paul Walters z5077446 for COMP9041 Assignment 1 Oct 2016
 
-use strict;
-use warnings;
-use diagnostics;
+# This interpreter converts basic perl code to python 3
+# It uses a recursive , regex-driven parsing method
+# to call functions corresponding to terminal and non-terminal
+# perl syntax
+
+# For details of the trasnlation engine and functions see plpy_engine.p,,
+# plpy_nonterminals.pm and plpy_terminals.pm
+
+#use strict;
+#use warnings;
+#use diagnostics;
 
 use FindBin;
 use lib "$FindBin::Bin";
-use plpy_functions;
+
 use plpy_engine;
 use plpy_terminals;
 use plpy_nonterminals;
 
-#my $indent = 0; #adding global indent variable
-#my @last_nested = ();	#lists most recent nesting
-#my $ind_sep = "   "; 	# variable containing three spaces, used for indenting.
-
 my @shebang_header = ();	# used to retrieve header from plpy_nonterminals
 my @python_text = ();		#Stores python code body as taken from plpy_engine
-
-#print plpy_engine::iterate_trans_functions('$hello')."\n";
-#print plpy_engine::iterate_trans_functions('@myhello')."\n";
-#print plpy_engine::iterate_trans_functions('%myhello')."\n";
-#print plpy_engine::iterate_trans_functions('78450067')."\n";
-#print plpy_engine::iterate_trans_functions('-756')."\n";
-#print plpy_engine::iterate_trans_functions('54.67')."\n";
-#print plpy_engine::iterate_trans_functions('-45.67')."\n";
-#print plpy_engine::iterate_trans_functions('679.67')."\n";
-#print plpy_engine::iterate_trans_functions('-.04754')."\n";
-#print plpy_engine::iterate_trans_functions("   ")."\n";
-#print plpy_engine::iterate_trans_functions('||')."\n";
-#print plpy_engine::iterate_trans_functions('&&')."\n";
-#print plpy_engine::iterate_trans_functions('+')."\n";
-#print plpy_engine::iterate_trans_functions('}')."\n";
-#print plpy_engine::iterate_trans_functions('next')."\n";
-#print plpy_engine::iterate_trans_functions('last')."\n";
-#print plpy_engine::iterate_trans_functions(';')."\n";
-#print plpy_engine::iterate_trans_functions('==')."\n";
-#print plpy_engine::iterate_trans_functions('!=')."\n";
-#print plpy_engine::iterate_trans_functions('!!')."\n";
-#print plpy_engine::iterate_trans_functions('chomp($hello)')."\n";
-#print plpy_engine::iterate_trans_functions('$left <=> $right')."\n";
-#print plpy_engine::iterate_trans_functions('!($test)')."\n";
-#print plpy_engine::iterate_trans_functions('($b)')."\n";
-#print plpy_engine::iterate_trans_functions('$temp == 6.5')."\n";
-#print plpy_engine::iterate_trans_functions('$a + 3 + (6 / 2)')."\n";
-#print plpy_engine::iterate_trans_functions('4 ** 12')."\n";
-#print plpy_engine::iterate_trans_functions('$a + x != 3 + chomp($r) + (12 % 2) = 12')."\n";
-#print plpy_engine::iterate_trans_functions('$a >> x != 3 | chomp($r) + (12 ^ 2) << 12')."\n";
-#print plpy_engine::iterate_trans_functions('if($x == 3){')."\n";
-#print plpy_engine::iterate_trans_functions('elsif(chomp($s))')."\n";
-#print plpy_engine::iterate_trans_functions('else{')."\n";
-#print plpy_engine::iterate_trans_functions('while(chomp($x) + 4 = 3){')."\n";
-#print plpy_engine::iterate_trans_functions('foreach $x (4..10){')."\n";
-#print plpy_engine::iterate_trans_functions('$x++;')."\n";
-#print plpy_engine::iterate_trans_functions('($x--)')."\n";
-#print plpy_engine::iterate_trans_functions('for ($x = 1;$x < 153; $x ++ )')."\n";
-#print plpy_engine::iterate_trans_functions('for ($x = 1546;$x > 153; $x -- )')."\n";
-#print plpy_engine::iterate_trans_functions("print(\'dgijdoijdoijv oiodhviodhoih\n\');")."\n";
-#print plpy_engine::iterate_trans_functions('print "dgijdoijdoijv oiodh viod hoih\n";')."\n";
-#print plpy_engine::iterate_trans_functions('print "dgijdoijdoijv $test", $i * $j."oiodh"."viod hoih\n";')."\n";
-#print plpy_engine::iterate_trans_functions('print "dgijdoijdoijv $1 $hello oio %hello2 dhv @hello3 iodhoih";')."\n";
-#print plpy_engine::iterate_trans_functions('$line1 .= $line2.line3')."\n";
-#print plpy_engine::iterate_trans_functions('print "Give me cookie/n";')."\n";
-#print plpy_engine::iterate_trans_functions('join(";",@texts)')."\n";
-#print plpy_engine::iterate_trans_functions('my $i = 45 + $z')."\n";
-#print plpy_engine::iterate_trans_functions('$i = <STDIN>')."\n";
-#print plpy_engine::iterate_trans_functions('split /;/,$hello2,3')."\n";
-#print plpy_engine::iterate_trans_functions('split /" + "/,@hello2')."\n";
-#print plpy_engine::iterate_trans_functions('$text1.$text2 eq $text3 + "hello"')."\n";
-#print plpy_engine::iterate_trans_functions('while ($line = <>) {')."\n";
-#print plpy_engine::iterate_trans_functions('$list[$item] = $t + 5;')."\n";
-#print plpy_engine::iterate_trans_functions('$list[5..23]')."\n";
-#print plpy_engine::iterate_trans_functions('my @mylist = [3,4,"hello1",$var1,@list1]')."\n";
-#print plpy_engine::iterate_trans_functions('$list[4,5,6,7]')."\n";
 
 #Loops through all STDIN
 while (my $line = <>) {
